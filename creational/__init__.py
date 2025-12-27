8b37727cd29e1226102061efb5137f5f3053476a
Hassan Tarek <hassanelashry415@gmail.com>
2025-12-27 14:13:20 +0200

    add __init__.py for creational package

---
+++ creational/__init__.py
@@ -0,0 +1,13 @@
+from .singleton import Singleton
+from .factory_method import Factory
+from .abstract_factory import AbstractFactory
+from .builder import Builder
+from .prototype import Prototype
+
+__all__ = [
+    "Singleton",
+    "Factory",
+    "AbstractFactory",
+    "Builder",
+    "Prototype",
+]
